70d0537281037928f499cc0199f4154834d5b964
Sukrian Syahnel Putra <rian.syahnel@gmail.com>
2017-11-18 05:27:47 +0700

    sign out at viewall

--- src/main/java/com/example/Tutorial08Application.java
+++ src/main/java/com/example/Tutorial08Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApapTutorial04Application
+public class Tutorial08Application
 {
 
     public static void main (String[] args)
     {
-        SpringApplication.run (ApapTutorial04Application.class, args);
+        SpringApplication.run (Tutorial08Application.class, args);
     }
 }
